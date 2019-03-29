26cdd27db088c8b56f736f3053900ce94501aec0
Nancy <nancy@logisticinfotech.co.in>
2019-03-29 12:14:28 +0530

    Initial commi

---
+++ BasicArithmetics/Classes/ArithmeticService.swift
@@ -0,0 +1,21 @@
+//
+//  ArithmeticService.swift
+//  BasicArithmetics
+//
+//  Created by Nancy on 29/03/19.
+//
+
+import Foundation
+
+public class ArithmeticService{
+    
+    public init() {}
+    
+    public func addition(number1: Int, number2: Int) -> Int{
+        return number1 + number2
+    }
+    
+    public func subtraction(number1: Int, number2: Int) -> Int{
+        return number1 - number2
+    }
+}
---
+++ Example/Pods/Target Support Files/BasicArithmetics/BasicArithmetics-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BasicArithmetics : NSObject
+@end
+@implementation PodsDummy_BasicArithmetics
+@end
